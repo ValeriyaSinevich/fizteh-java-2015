c00906a1be3883e8b303e1702a4e719510842c3b
ValeriyaSinevich <Valeriya.Sinevich@phystech.edu>
2015-10-07 11:38:37 +0300

    autosave

--- ValeriyaSinevich/src/test/java/ru/fizteh/fivt/students/ValeriyaSinevich/modultests/library/PrinterTest.java
+++ ValeriyaSinevich/src/test/java/ru/fizteh/fivt/students/ValeriyaSinevich/modultests/library/PrinterTest.java
@@ -1,13 +1,43 @@
 package ru.fizteh.fivt.students.ValeriyaSinevich.modultests.library;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+import twitter4j.QueryResult;
+import twitter4j.Status;
 
+import java.util.List;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.argThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-/**
- * Created by root on 10/7/15.
- */
+@RunWith(MockitoJUnitRunner.class)
 public class PrinterTest {
+    @Before
+    public void setUp() throws Exception {
+        ParametersParser parser = mock(ParametersParser.class);
+        Status tweet = mock(Status.class);
+        List<Status> = mock(List.class);
+
+        when(translator.translateMinutes()).thenReturn(statuses);
+
+        when(twitter.search(argThat(hasProperty("query", equalTo("java")))))
+                .thenReturn(queryResult);
+
+        QueryResult emptyQueryResult = mock(QueryResult.class);
+        when(emptyQueryResult.getTweets()).thenReturn(Collections.emptyList());
+
+        when(twitter.search(argThat(hasProperty("query", not(equalTo("java"))))))
+                .thenReturn(emptyQueryResult);
+    }
+
+
 
     @Test
     public void testPrintTweet() throws Exception {
--- ValeriyaSinevich/src/test/java/ru/fizteh/fivt/students/ValeriyaSinevich/modultests/library/RussianTest.java
+++ ValeriyaSinevich/src/test/java/ru/fizteh/fivt/students/ValeriyaSinevich/modultests/library/RussianTest.java
@@ -1,13 +1,37 @@
 package ru.fizteh.fivt.students.ValeriyaSinevich.modultests.library;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+import twitter4j.QueryResult;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.argThat;
+import static org.mockito.Mockito.*;
 
 import static org.junit.Assert.*;
 
-/**
- * Created by root on 10/7/15.
- */
+@RunWith(MockitoJUnitRunner.class)
 public class RussianTest {
+
+    @Before
+    public void setUp() throws Exception {
+        Russian translator = mock(Russian.class);
+
+        when(translator.translateMinutes()).thenReturn(statuses);
+
+        when(twitter.search(argThat(hasProperty("query", equalTo("java")))))
+                .thenReturn(queryResult);
+
+        QueryResult emptyQueryResult = mock(QueryResult.class);
+        when(emptyQueryResult.getTweets()).thenReturn(Collections.emptyList());
+
+        when(twitter.search(argThat(hasProperty("query", not(equalTo("java"))))))
+                .thenReturn(emptyQueryResult);
+    }
 
     @Test
     public void testTranslateMinutes() throws Exception {
